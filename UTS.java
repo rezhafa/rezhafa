c6fc4ec73da112040774225fbf385ad87c5a18ed
rezhafa <70991834+rezhafa@users.noreply.github.com>
2020-09-09 09:51:00 +0700

    Add files via upload

---
+++ UTS.java
@@ -0,0 +1,27 @@
+package reza;
+
+public class UTS {
+
+	public static void main(String[] args) {
+		String ejut = "ABCDEFGHIJKLMNOPQRSTUVWXYZ-# ";
+		char name1 = ejut.charAt(17);
+		char name2 = ejut.charAt(4);
+		char name3 = ejut.charAt(25);
+		char name4 = ejut.charAt(7);
+		char name5 = ejut.charAt(0);
+		
+		char name6 = ejut.charAt(5);
+		char name7 = ejut.charAt(0);
+		char name8 = ejut.charAt(7);
+		char name9 = ejut.charAt(11);
+		char name10 = ejut.charAt(4);
+		char name11 = ejut.charAt(21);
+		char name12 = ejut.charAt(24);
+		
+		
+		System.out.println("" + name1 + name2 + name3 + name4 + name5 + " " 
+		+ name6 + name7 + name8 + name9 + name10 + name11 + name12);
+
+	}
+
+}
